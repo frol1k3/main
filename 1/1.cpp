8ea0f40d4e5aab4ed5b7dd2529192ba25989febf
frol1k3 <alex_frolov24042007@mail.ru>
2023-09-08 19:15:18 +0300

    DZ

---
+++ 1/1.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <random>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	while (true) {
+		
+		string phrase = "";
+
+		cout << "";
+		cin >> phrase;
+		if (phrase == "Привет" || phrase == "Здравстуйте" || phrase == "привет" || phrase == "здравстуйте") {
+			cout << "Привет!" << "\n";
+		}
+		else if (phrase == "Пока" || phrase == "пока") {
+			cout << "Пока!";
+			break;
+		}
+		else {
+			int min = 1;
+			int max = 3;
+			int n = rand() % max + min;
+			if (n == 1) {
+				cout << "Я абсолют" << "\n";
+			}
+			else if (n == 2) {
+				cout << "Я не Тайлер Дердан" << "\n";
+			}
+			else if (n == 3) {
+				cout << "Раян Гослинг" << "\n";
+			}
+		}
+	}
+}
--- main/main.cpp
+++ main/main.cpp
@@ -1,5 +1,4 @@
 ﻿#include <iostream>
-#include <random>
 
 using namespace std;
 
@@ -7,19 +6,5 @@ int main()
 {	
 	setlocale(LC_ALL, "");
 
-	while (true){
-		int a;
-		cout << "1)Бросить кости, 2)Выйти: ";
-		cin >> a;
-		if (a == 1) {
-			cout <<"Выпало " << rand() % 6 + 1 << "\n";
-		}
-		else if (a == 2) {
-			cout << "Вы вышли";
-			break;
-		}
-		else {
-			cout << "Неправильный ввод";
-		}
-	}
+	
 }
